df440cc1e9392599ef6985eb47c039cff028f90c
Kristijan Korać <korac.kristijan@gmail.com>
2016-11-29 14:26:24 +0100

    starting splash screen added; checking the data in the registry in the second thread

---
+++ INFOS-Project_X/Splash.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace INFOS_Project_X
+{
+    partial class Splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNaslov = new System.Windows.Forms.Label();
+            this.lblSpajanje = new System.Windows.Forms.Label();
+            this.bworker = new System.ComponentModel.BackgroundWorker();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNaslov
+            // 
+            this.lblNaslov.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNaslov.AutoSize = true;
+            this.lblNaslov.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblNaslov.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblNaslov.Location = new System.Drawing.Point(282, 9);
+            this.lblNaslov.Name = "lblNaslov";
+            this.lblNaslov.Size = new System.Drawing.Size(256, 31);
+            this.lblNaslov.TabIndex = 0;
+            this.lblNaslov.Text = "INFOS - Project_X";
+            // 
+            // lblSpajanje
+            // 
+            this.lblSpajanje.AutoSize = true;
+            this.lblSpajanje.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSpajanje.Location = new System.Drawing.Point(12, 341);
+            this.lblSpajanje.Name = "lblSpajanje";
+            this.lblSpajanje.Size = new System.Drawing.Size(139, 17);
+            this.lblSpajanje.TabIndex = 1;
+            this.lblSpajanje.Text = "Spajanje na server...";
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(12, 328);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(48, 13);
+            this.lblProgress.TabIndex = 2;
+            this.lblProgress.Text = "Progress";
+            // 
+            // Splash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::INFOS_Project_X.Properties.Resources.water_splash_png_23;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(550, 367);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.lblSpajanje);
+            this.Controls.Add(this.lblNaslov);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Splash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Splash";
+            this.Load += new System.EventHandler(this.Splash_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNaslov;
+        private System.Windows.Forms.Label lblSpajanje;
+        private System.ComponentModel.BackgroundWorker bworker;
+        private System.Windows.Forms.Label lblProgress;
+    }
+}
---
+++ INFOS-Project_X/Splash.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace INFOS_Project_X
+{
+    public partial class Splash : Form
+    {
+        bool serverOk = false;
+        public Splash()
+        {
+            InitializeComponent();
+
+            bworker.DoWork                              += Bworker_DoWork;
+            bworker.RunWorkerCompleted                  += Bworker_RunWorkerCompleted;
+        }
+
+        private void Splash_Load(object sender, EventArgs e)
+        {
+            this.Cursor                                 = Cursors.WaitCursor;
+            bworker                                     .RunWorkerAsync();
+        }
+
+        private void Bworker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!serverOk)
+            {
+                Console.WriteLine("Error with server connection");
+
+                ServerAuthForm authForm     = new ServerAuthForm();
+                Thread                      .Sleep(2000);
+
+                if (authForm.ShowDialog(this) == DialogResult.OK)
+                {
+                    this.DialogResult       = DialogResult.OK;
+                }
+                this.Cursor                 = Cursors.Default;
+            }
+            else
+            {
+                Thread.Sleep(2000);
+                this.DialogResult           = DialogResult.OK;
+            }
+        }
+
+        private void Bworker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                for (int i = 1; i <= 1000; i++)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        lblProgress.Text    = i.ToString() + " / 1000";
+                    }));
+                }
+
+                this.Invoke(new Action(() =>
+                {
+                    Change_Status("Prikupljam podatke o serveru...");
+                    lblNaslov               .Refresh();
+                    this                    .Invalidate();
+                }));
+
+                RegistryKey infosKey        = Registry.CurrentUser.OpenSubKey("SOFTWARE\\C#\\Infos-ProjectX");
+
+                string server               = (string)infosKey.GetValue("Server");
+                string database             = (string)infosKey.GetValue("database");
+                string username             = (string)infosKey.GetValue("username");
+                string password             = (string)infosKey.GetValue("password");
+                bool winAuth                = false;
+
+                string connectionString     = "Data Source=" + server
+                                            + ";Initial Catalog=" + database
+                                            + ";Integrated Security=" + winAuth.ToString();
+
+                if (!winAuth)
+                {
+                    connectionString        += ";User ID=" + username
+                                            + ";Password=" + password
+                                            + ";";
+                }
+                else
+                {
+                    connectionString        += ";";
+                }
+
+                this.Invoke(new Action(() =>
+                {
+                    Change_Status("Pokušavam ostvariti konekciju s bazom...");
+                }));
+
+                SqlConnection infosConnection       = new SqlConnection(connectionString);
+
+                infosConnection                     .Open();
+
+                Console                             .WriteLine("State {0}", infosConnection.State);
+                Console                             .WriteLine("Connection string: {0}", infosConnection.ConnectionString);
+
+                this.Invoke(new Action(() =>
+                {
+                    Change_Status("Konekcija uspješno ostvarena...");
+                }));
+
+                infosConnection                     .Close();
+
+                RegistryKey infosToRegistry         = Registry.CurrentUser.CreateSubKey("SOFTWARE\\C#\\Infos-ProjectX");
+
+                infosToRegistry                     .SetValue("Server", server);
+                infosToRegistry                     .SetValue("Database", database);
+
+                PomocneFunkcije.connectionString    = connectionString;
+
+                serverOk                            = true;
+
+                this.Invoke(new Action(() =>
+                {
+                    Change_Status("Server spreman...Pristupam glavnom programu..");
+                }));
+
+            }
+            catch
+            {
+                serverOk = false;
+            }
+        }
+
+        private void Change_Status(string status)
+        {
+            lblSpajanje.Text    = status;
+            lblSpajanje         .Refresh();
+            Thread              .Sleep(1000);
+        }
+        
+    }
+}
